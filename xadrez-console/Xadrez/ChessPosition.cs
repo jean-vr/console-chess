f55c6dda13c50ecf3450f38a21d4967fb9d8e59e
Jean Ribeiro <jeanvr098@gmail.com>
2020-09-07 19:22:47 -0300

    Criando a classe responsável por converter posição de chadrez para posição de matriz

---
+++ xadrez-console/Xadrez/ChessPosition.cs
@@ -0,0 +1,21 @@
+﻿using Tabuleiro;
+
+namespace Xadrez {
+    class ChessPosition {
+        public char column { get; set; }
+        public int line { get; set; }
+
+        public ChessPosition(char col, int lin) {
+            column = col;
+            line = lin;
+        }
+
+        public Position ToPosition() {
+            return new Position(8 - line, column - 'a');
+        }
+
+        public override string ToString() {
+            return "" + column + line;
+        }
+    }
+}
